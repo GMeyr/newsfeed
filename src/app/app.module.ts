5690e135a48abf69d19987806270088bebcb1918
Gabriel Meyr <gmeyr@Gabriels-MacBook-Pro.local>
2019-07-31 20:19:47 -0500

    feat: Break itemlist into own component:wq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ItemlistComponent } from './itemlist/itemlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ItemlistComponent
   ],
   imports: [
     BrowserModule

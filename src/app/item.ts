b8c88376261ff28f2d86d8582d5ed534e9703d85
Gabriel Meyr <gmeyr@Gabriels-MacBook-Pro.local>
2019-08-06 07:53:29 -0500

    feat: Break Item out into its own component

---
+++ src/app/item.ts
@@ -0,0 +1,8 @@
+export class Item {
+    img: string;
+    title: string;
+    post: string;
+    post_abbr: string;
+    author: string;
+    date: string;
+}
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent {
+    @Input() item: Item;
 
-  constructor() { }
-
-  ngOnInit() {
+    constructor() {}
   }
-
-}
